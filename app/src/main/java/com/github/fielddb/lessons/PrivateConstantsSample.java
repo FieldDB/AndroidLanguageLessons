0c1356903bd2c45fdd4df427b84d03f7db5e22f3
cesine <cesine@yahoo.com>
2018-11-18 16:23:16 -0500

    travis

---
+++ app/src/main/java/com/github/fielddb/lessons/PrivateConstantsSample.java
@@ -0,0 +1,14 @@
+package com.github.fielddb.lessons;
+
+/**
+ * http://developer.android.com/tools/devices/emulator.html#networkaddresses
+ * 10.0.2.2 emulator connection to localhost
+ */
+public class PrivateConstantsSample extends com.github.fielddb.PrivateConstantsSample {
+  public static String APP_TYPE = "speechrecognition";
+
+  public static String DATA_IS_ABOUT_LANGUAGE_ISO = "ka";
+  public static String DATA_IS_ABOUT_LANGUAGE_NAME_ASCII = "kartuli";
+  public static String DATA_IS_ABOUT_LANGUAGE_NAME = "ქართული";
+  public static String USER_FRIENDLY_DATA_NAME = "ფრაზა";
+}
